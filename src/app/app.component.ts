5182c388095ce66d033ee05cc5ee4185c60e5c23
dean-mcbean <dean.w.mcbean@gmail.com>
2024-02-03 18:46:27 +1300

    Wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from '../components/molecules/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { DashboardPage } from '../components/pages/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'dashboard', component: DashboardPage },
+  // default route
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+];
